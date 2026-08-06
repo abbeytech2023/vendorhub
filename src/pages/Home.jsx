132601f1720eae0967a332084325c16ce08275e8
olanipekun abiodun <olanipekunabbey@gmail.com>
2026-08-06 15:16:14 -0700

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -62,7 +62,7 @@ export default function Home() {
         </section>
 
         <ProductsSection />
-        <ServiceProviders />
+        {/* <ServiceProviders /> */}
         <Features />
         <Footer />
       </div>
